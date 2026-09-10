c4dee103aeff55a3dcdf739b0be1b36c2be167b8
abwmrwtaltmymy-blip <abwmrwtaltmymy@gmail.com>
2026-09-10 17:00:39 +0300

    Refactor environment variable loading functions

--- config.py
+++ config.py
@@ -3,9 +3,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
-DEV_ID = int(os.getenv("DEV_ID", 0))
+def _get_int(name, default=0):
+    val = os.getenv(name)
+    if val is None or val == "":
+        return default
+    try:
+        return int(val)
+    except ValueError:
+        return default
+
+def _get_str(name, default=""):
+    val = os.getenv(name)
+    if val is None:
+        return default
+    return val
+
+API_ID = _get_int("API_ID", 0)
+API_HASH = _get_str("API_HASH", "")
+BOT_TOKEN = _get_str("BOT_TOKEN", "")
+GEMINI_API_KEY = _get_str("GEMINI_API_KEY", "")
+DEV_ID = _get_int("DEV_ID", 0)
 ADMIN_ID = DEV_ID
