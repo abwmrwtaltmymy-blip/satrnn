7b8976d4d55f4160403d183c7046fc2c4481da9e
abwmrwtaltmymy-blip <abwmrwtaltmymy@gmail.com>
2026-09-11 02:11:27 +0300

    Change initial team points from 60 to 3

--- game_manager.py
+++ game_manager.py
@@ -28,8 +28,8 @@ class InternalGame:
         self.names = []
         self.team1 = []
         self.team2 = []
-        self.team1_points = 60
-        self.team2_points = 60
+        self.team1_points = 3
+        self.team2_points = 3
         self.round = 1
         self.state = "waiting"
         self.question = None
