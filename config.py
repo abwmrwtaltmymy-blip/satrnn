d55ebcfd730732d5e581e7e3731c8d56110ab3f0
abwmrwtaltmymy-blip <abwmrwtaltmymy@gmail.com>
2026-09-10 16:55:55 +0300

    Replace ADMIN_ID with DEV_ID assignment

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+DEV_ID = int(os.getenv("DEV_ID", 0))
+ADMIN_ID = DEV_ID
