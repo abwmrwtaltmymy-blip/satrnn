b2097f861baac1df7566361e2acc67718ee5ff6b
abwmrwtaltmymy-blip <abwmrwtaltmymy@gmail.com>
2026-09-15 03:48:59 +0300

    Add developer username, channel, and bio to config

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ API_HASH = os.getenv("API_HASH", "31a2abacece3f047a878d001aa3fbd95")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 DEV_ID = int(os.getenv("DEV_ID", "7367921416"))
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+DEV_USERNAME = "aakaaz"
+DEV_CHANNEL = "sa00cr"
+DEV_BIO = "ساترن"
